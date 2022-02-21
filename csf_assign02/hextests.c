beedaf29bf07fac5f12ffa9f904976906c620f17
heyitsM <eberge11@jhu.edu>
2022-02-21 13:12:16 -0500

    Added tests for testFormatOffset and testFormatByteAsHex

--- csf_assign02/hextests.c
+++ csf_assign02/hextests.c
@@ -57,12 +57,67 @@ void testFormatOffset(TestObjs *objs) {
 
   hex_format_offset(16L, buf);
   ASSERT(0 == strcmp(buf, "00000010"));
+
+  hex_format_offset(8L, buf);
+  ASSERT(0 == strcmp(buf, "00000008"));
+
+  hex_format_offset(10L, buf);
+  ASSERT(0 == strcmp(buf, "0000000a"));
+
+  hex_format_offset(256L, buf);
+  ASSERT(0 == strcmp(buf, "00000100"));
+
+  hex_format_offset(0x11111111L, buf);
+  ASSERT(0 == strcmp(buf, "11111111"));
+
+  hex_format_offset(0xffffffffL, buf);
+  ASSERT(0 == strcmp(buf, "ffffffff"));
 }
 
 void testFormatByteAsHex(TestObjs *objs) {
   char buf[16];
+  //H
   hex_format_byte_as_hex(objs->test_data_1[0], buf);
   ASSERT(0 == strcmp(buf, "48"));
+  //e
+  hex_format_byte_as_hex(objs->test_data_1[1], buf);
+  ASSERT(0 == strcmp(buf, "65"));
+  //l
+  hex_format_byte_as_hex(objs->test_data_1[2], buf);
+  ASSERT(0 == strcmp(buf, "6c"));
+  //l
+  hex_format_byte_as_hex(objs->test_data_1[3], buf);
+  ASSERT(0 == strcmp(buf, "6c"));
+  //o
+  hex_format_byte_as_hex(objs->test_data_1[4], buf);
+  ASSERT(0 == strcmp(buf, "6f"));
+  //,
+  hex_format_byte_as_hex(objs->test_data_1[5], buf);
+  ASSERT(0 == strcmp(buf, "2c"));
+  //space
+  hex_format_byte_as_hex(objs->test_data_1[6], buf);
+  ASSERT(0 == strcmp(buf, "20"));
+  //w
+  hex_format_byte_as_hex(objs->test_data_1[7], buf);
+  ASSERT(0 == strcmp(buf, "77"));
+  //o
+  hex_format_byte_as_hex(objs->test_data_1[8], buf);
+  ASSERT(0 == strcmp(buf, "6f"));
+  //r
+  hex_format_byte_as_hex(objs->test_data_1[9], buf);
+  ASSERT(0 == strcmp(buf, "72"));
+  //l
+  hex_format_byte_as_hex(objs->test_data_1[10], buf);
+  ASSERT(0 == strcmp(buf, "6c"));
+  //d
+  hex_format_byte_as_hex(objs->test_data_1[11], buf);
+  ASSERT(0 == strcmp(buf, "64"));
+  //!
+  hex_format_byte_as_hex(objs->test_data_1[12], buf);
+  ASSERT(0 == strcmp(buf, "21"));
+  //\n but actually .
+  hex_format_byte_as_hex(objs->test_data_1[13], buf);
+  ASSERT(0 == strcmp(buf, "0a"));
 }
 
 void testHexToPrintable(TestObjs *objs) {
