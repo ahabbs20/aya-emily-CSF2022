e74855c240effb889ee520303aad78f522597f37
ahabbs20 <a.habbs20@gmail.com>
2022-03-07 21:55:11 -0500

    Edited Makefile, deleted unnecessary files

--- csf_assign03/cache_func.cpp
+++
@@ -1,5 +0,0 @@
-// Main Cache function
-/* Emily Berger - eberge11@jh.edu
-   Aya Habbas - ahabbas1@jh.edu */
-
-
--- csf_assign03/cache_funcs.h
+++
@@ -1,8 +0,0 @@
-// Function prototypes for hexdump functions
-// Do not change these!
-
-#ifndef CACHE_FUNCS_H
-#define CACHE_FUNCS_H
-
-
-#endif // CACHE_FUNCS_H
