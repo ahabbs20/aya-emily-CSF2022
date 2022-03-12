89599cb1151550ea95d0453682701ddc7f625728
ahabbs20 <a.habbs20@gmail.com>
2022-03-11 23:34:20 -0500

    Finally starting cache
    Co-authored-by: Emily Berger <heyitsM@users.noreply.github.com>

--- csf_assign03/cache_main.cpp
+++ csf_assign03/cache_main.cpp
@@ -26,6 +26,15 @@ class Slot {
 
 
    public: 
+      Slot() {
+         tag = 0;
+         load_ts = 0;
+         access_ts = 0;
+         valid = false;
+         dirty = false;
+
+      }
+
       Slot(unsigned int tag, unsigned int load_ts, unsigned int access_ts): tag(tag), load_ts(load_ts), access_ts(access_ts) {
          valid = true;
       }
@@ -52,16 +61,28 @@ class Set {
 
    public:
 
-      Set(int size, int m_slots) : set(size), max_slots(m_slots) { }
+      Set(int size, int m_slots) : max_slots(m_slots), set(size){ 
+         vector<Slot> temp(size);
+         set = temp;
 
+      }
+      
       int max_slots;
       vector<Slot> set;
 };
 
 class Cache {
    /* TODO:
-      var that tracks how much of the address to read for tag
-      var that tracks how much of address to read for index
+      EMILY!!!! LOOK OVER HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      Emily's Tasks: 
+         - store
+         - find
+
+      Aya's Tasks: 
+         - Load
+         - evict_Lru
+         - evict_Fifo
 
    */
 
@@ -75,30 +96,102 @@ class Cache {
       
       //Probably main constructor
       Cache(int num_s, int num_b, int block_s, bool * write_a, bool * write_th): 
-         num_sets(num_s), num_blocks(num_b), block_size(block_s), write_allocate(*write_a), write_through(*write_th) {};
+         num_sets(num_s), num_blocks(num_b), block_size(block_s), write_allocate(*write_a), write_through(*write_th) {
+           //Add vector in vectors here lmao
+           bits_in_offset = log_2(block_size, 0);
+           bits_in_index = log_2(num_blocks, 0);
+           bits_in_tag = 64 - bits_in_offset - bits_in_index;
+
+           vector<Set> temp(num_s);
+           cache = temp;
+         };
          
-      unsigned int decodeTag(unsigned int address) {
-         return 0;
+      
+      //you have number of sets (num_sets)
+      //number of blocks (num_blocks)
+      //block size (block_size)
+
+      //tag index offset
+      unsigned int decode_tag(unsigned int address) {
+         //shift out offset bits
+         if (bits_in_offset + bits_in_index == 64) { // undefined behaviour, must be edge case
+            return 0;
+         }
+         address = address >> (bits_in_offset + bits_in_index);
+
+         return address;
       }
       
       unsigned int decode_index(unsigned int address) {
-         return 0;
+         if (bits_in_offset + bits_in_tag == 64) { // undefined behaviour, must be edge case.
+            return 0;            
+         }
+
+         address = address >> bits_in_offset;
+         address = address << bits_in_offset + bits_in_tag;
+         address = address >> bits_in_offset + bits_in_tag;
+
+         return address;
       }
 
+      
       void load(unsigned int tag, unsigned int index) {
-         
+         /*
+          go to index, look for slot
+          if slot is found, do nothing
+          if slot if not found;
+            if (number of valid blocks in the set != max number of possible blocks) {
+             add it
+            } else {
+               // if (LRU) {
+                  remove_lru()
+                  add it
+               } else {
+                  remove_fifo
+                  add it
+               }
+            }
+         */
       }
+
+      /*
+         remove lru
+            set lru = first slot
+            iterate thru
+               if strictly less than, replace lru
+            delete lru 
+
+
+         remove fifo
+            set first in = first slot
+            iterate thru
+               if strictly less than, replace first in
+            delete first in
+
+      */
+
 
       void write(unsigned int tag, unsigned int index) {
+         /*
+            if (hit) {
+               if (write back)
+                  mark dirty
+            else 
+               if write-allocate
+                  load()
+               else {}
+            }
 
+
+         */
       }
 
    private:
-      bool write_allocate;
-      bool write_through;
       int num_sets;
       int num_blocks;
       int block_size;
+      bool write_allocate;
+      bool write_through;
       vector<Set> cache;
 
       int load_hit_count = 0;
@@ -107,21 +200,26 @@ class Cache {
       int store_miss_count = 0;
       int total_cycle = 0;      
 
-};
+      unsigned int bits_in_offset = 0;
+      unsigned int bits_in_index = 0;
+      unsigned int bits_in_tag = 0;
 
+      int log_2(int num, int result) {
+         while (num != 1) {
+            num = num >> 1;
+            result++;
+         }
+         return result;
+      }
+
+};
 bool isPowerOfTwo(int num) {
    return ((num & (num - 1)) == 0) && (num != 0);
 }
 
+string validate(int argc, int num_sets, int num_blocks, int block_size, bool *writeBack, bool *writeAllocate, string result1, string result2); 
+
 int main(int argc, char *argv[]) {
-   // number of sets in the cache
-   // number of blocks in each set
-   // number of bytes in each block
-   // write-allocate or no-write allocate
-   // write through or write back
-   // lru or fifo
-
-
    int numSets = atoi(argv[1]);
    int numBlocks = atoi(argv[2]);
    int blockSize = atoi(argv[3]);
@@ -137,27 +235,32 @@ int main(int argc, char *argv[]) {
    }
 
 
-   Cache cache = Cache(numSets, numBlocks, blockSize, writeAllocate, writeBack);
+   Cache cache = Cache(numSets, numBlocks, blockSize, &writeAllocate, &writeBack);
 
    char  op;
-   string adrs;
-   string theRest;
+   char * adrs;
+   char theRest;
 
    unsigned int tag;
    unsigned int index;
+   unsigned int address;
    
    while (cin.peek() != EOF) {
-      cin >> operation >> address >> theRest;
-      unsigned int address = atoi(adrs);
+      cin.clear();
+      cin >> op >> adrs >> theRest;
+      address = strtoul(adrs, NULL, 16);
       tag = cache.decode_tag(address);
       index = cache.decode_index(address);
 
+      cout << tag << " " << index << "\n";
+      /*
       if (op == 'l') {
          cache.load(tag, index);
       } else {
          cache.store(tag, index);
-      }
+      }*/
 
+      
    }
 
    /*
@@ -175,6 +278,10 @@ string validate(int argc, int num_sets, int num_blocks, int block_size, bool *wr
 
    if (!isPowerOfTwo(num_sets)) {
       return "Error: Number of sets must be power of two.\n";
+   }
+
+   if (!isPowerOfTwo(num_blocks)) {
+      return "Error: Number of blocks must be power of two.\n";
    }
 
    if (block_size < 4) {
