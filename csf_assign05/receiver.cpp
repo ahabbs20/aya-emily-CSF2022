90548019d49e79d4f63c0c4ae3c9b9795b9ba788
ahabbs20 <a.habbs20@gmail.com>
2022-04-20 14:53:43 -0400

    At a better point

--- csf_assign05/receiver.cpp
+++ csf_assign05/receiver.cpp
@@ -7,27 +7,76 @@
 #include "connection.h"
 #include "client_util.h"
 
+using std::cerr;
+using std::string;
+using std::endl;
+using std::cin;
+using std::cout;
+
 int main(int argc, char **argv) {
   if (argc != 5) {
-    std::cerr << "Usage: ./receiver [server_address] [port] [username] [room]\n";
+    cerr << "Usage: ./receiver [server_address] [port] [username] [room]\n";
     return 1;
   }
 
-  std::string server_hostname = argv[1];
+  string server_hostname = argv[1];
   int server_port = std::stoi(argv[2]);
-  std::string username = argv[3];
-  std::string room_name = argv[4];
+  string username = argv[3];
+  string room_name = argv[4];
 
   Connection conn;
+  conn.connect(server_hostname, server_port);
+  if (!conn.is_open()) {
+    cerr << "Unable to connect with: " << server_hostname << " and " << server_port << endl;
+    return -1;
+  }
 
-  // TODO: connect to server
+  Message return_msg;
+  bool result_of_last_connection;
+  
+  // Send slogin message
+  result_of_last_connection = conn.send(Message(TAG_RLOGIN, username));
+  if (!result_of_last_connection) {
+    std::cerr << "ERROR: unable to login with username " << username << endl;  
+    return -1;
+  }
+  
+  // read response: If not ok:
+  result_of_last_connection = conn.receive(return_msg);
+  if (return_msg.tag.compare(TAG_ERR) == 0) {
+    cerr << "ERROR: " << return_msg.data << endl;
+    return -1;
+  }
 
-  // TODO: send rlogin and join messages (expect a response from
-  //       the server for each one)
+  result_of_last_connection = conn.send(Message(TAG_JOIN, room_name));
+  if (!result_of_last_connection) {
+    std::cerr << "ERROR: Unable to join due to EOF or other Error" << endl;  
+    return -1;
+  }
 
-  // TODO: loop waiting for messages from server
-  //       (which should be tagged with TAG_DELIVERY)
+  result_of_last_connection = conn.receive(return_msg);
+  if (return_msg.tag.compare(TAG_ERR) == 0) {
+    cerr << "ERROR: " << return_msg.data << endl;
+    return -1;
+  } else if (!result_of_last_connection) {
+    cerr << "Unable to perform action: Join." << endl;
+    return -1;
+  }
 
+  while (1) {
+    result_of_last_connection = conn.receive(return_msg);
+    if (!result_of_last_connection) {
+      cerr << "Unable to perform receive message" << endl; // fatal error
+      return -1;
+    } else if (return_msg.tag.compare(TAG_ERR) == 0) {
+      cerr << "ERROR: " << return_msg.data << endl;
+    } else if (return_msg.tag.compare(TAG_DELIVERY) == 0) {
+      std::vector<string> payload = return_msg.split_payload();
+      cout << payload[1] << ": " << payload[2] << endl;
+    }
+  }
+  
+  conn.close();
 
   return 0;
 }
--- csf_assign05/sender.cpp
+++ csf_assign05/sender.cpp
@@ -7,26 +7,103 @@
 #include "connection.h"
 #include "client_util.h"
 
+using std::cerr;
+using std::string;
+using std::endl;
+using std::cin;
+
 int main(int argc, char **argv) {
   if (argc != 4) {
-    std::cerr << "Usage: ./sender [server_address] [port] [username]\n";
+    cerr << "Usage: ./sender [server_address] [port] [username]\n";
     return 1;
   }
 
-  std::string server_hostname;
+  string server_hostname;
   int server_port;
-  std::string username;
+  string username;
 
   server_hostname = argv[1];
   server_port = std::stoi(argv[2]);
   username = argv[3];
 
-  // TODO: connect to server
+  // Connection to port56
+  Connection conn;
+  conn.connect(server_hostname, server_port);
+  if (!conn.is_open()) {
+    cerr << "Unable to connect with: " << server_hostname << " and " << server_port << endl;
+    return -1;
+  }
 
-  // TODO: send slogin message
+  Message return_msg;
+  bool result_of_last_connection;
+  
+  // Send slogin message
+  result_of_last_connection = conn.send(Message(TAG_SLOGIN, username));
+  if (!result_of_last_connection) {
+    std::cerr << "ERROR: unable to login with username " << username << endl;  
+    return -1;
+  }
+  
+  // read response: If not ok:
+  result_of_last_connection = conn.receive(return_msg);
+  if (return_msg.tag.compare(TAG_ERR) == 0) {
+    cerr << "ERROR: " << return_msg.data << endl;
+    return -1;
+  }
 
-  // TODO: loop reading commands from user, sending messages to
-  //       server as appropriate
+  string input;
+  while (1) {
+      cin >> input;
+    if (input[0] != '/') { // check if it is not a command
+      //rest of message
+      string rest;
+      std::getline(cin, rest);
+      input += rest;
+      result_of_last_connection = conn.send(Message(TAG_SENDALL, input));
 
+    } else {
+      if (input.compare("/join") == 0) { // join request
+        string room;
+        cin >> room; 
+        if (room.compare("") == 0) {
+          cerr << "ERROR: Join request must be of format \"/join [room]\"\n";
+        }
+        result_of_last_connection = conn.send(Message(TAG_JOIN, room));
+
+      } else if (input.compare("/leave") == 0) {  // leave request
+        result_of_last_connection = conn.send(Message(TAG_LEAVE, ""));
+
+      } else if (input.compare("/quit") == 0) { // quit request
+        result_of_last_connection = conn.send(Message(TAG_QUIT, ""));
+        result_of_last_connection = conn.receive(return_msg);
+        if (return_msg.tag.compare(TAG_OK) == 0) { // if can leave, just leave
+          return -1;
+        } else if (return_msg.tag.compare(TAG_ERR) == 0) { // else pass error
+          cerr << "ERROR: Unable to quit, killing program\n";
+          return -1;
+        }
+
+      } else { // cannot parse unknown command
+        std::cerr << "ERROR: Unknown command, please use on of the following: /join, /leave, /quit\n";
+
+      }
+    }
+ 
+    if (conn.get_last_result() != conn.SUCCESS) { // check to see if error arose from writing to server
+      cerr << "ERROR: Unable to send message\n";
+      return -2; // connection on 
+    }
+    
+    // 
+    result_of_last_connection = conn.receive(return_msg);
+
+    if (return_msg.tag.compare(TAG_ERR) == 0) { // check if server sent any error
+      cerr << "ERROR: " << return_msg.data << endl;
+    }
+
+    // empty buffer
+    std::cout.flush();
+  }
+  
   return 0;
 }
