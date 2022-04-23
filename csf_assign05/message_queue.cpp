78fa52bb20327a10e5b4c21f5f24ae91f806bd17
heyitsM <eberge11@jhu.edu>
2022-04-23 19:06:20 -0400

    quick

--- csf_assign05/message_queue.cpp
+++ csf_assign05/message_queue.cpp
@@ -4,17 +4,22 @@
 
 MessageQueue::MessageQueue() {
   // TODO: initialize the mutex and the semaphore
+  pthread_mutex_init(&m_lock, NULL);
+  sem_init(&m_avail, 0, 0);
 }
 
 MessageQueue::~MessageQueue() {
   // TODO: destroy the mutex and the semaphore
+  pthread_mutex_destroy(&m_lock);
+  sem_destroy(&m_avail);
 }
 
 void MessageQueue::enqueue(Message *msg) {
   // TODO: put the specified message on the queue
-
   // be sure to notify any thread waiting for a message to be
   // available by calling sem_post
+
+  m_messages.push_back(msg);
 }
 
 Message *MessageQueue::dequeue() {
@@ -31,8 +36,14 @@ Message *MessageQueue::dequeue() {
 
   // TODO: call sem_timedwait to wait up to 1 second for a message
   //       to be available, return nullptr if no message is available
+  sem_timedwait(&m_avail , &ts);
 
-  // TODO: remove the next message from the queue, return it
-  Message *msg = nullptr;
-  return msg;
+  // TODO: remove the next message frqom the queue, return it
+  if (!m_messages.empty()) {
+    Message *msg = m_messages.front(); //used to be Message *msg = nullptr
+    m_messages.pop_front();
+    return msg;
+  }
+   
+  return nullptr;
 }
